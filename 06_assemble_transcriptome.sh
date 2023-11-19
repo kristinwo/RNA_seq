42cb10ee670ec45b9e317d04f69d3330278384ea
kristinwo <kristin@watnedal.com>
2023-11-19 14:26:01 +0100

    Added merging of all gtf files

--- 06_assemble_transcriptome.sh
+++ 06_assemble_transcriptome.sh
@@ -17,10 +17,15 @@ BAM_FILE_DIR=/data/users/kolsen/rna_seq/bam_files
 REFERENCE_FILE_DIR=/data/users/kolsen/rna_seq/reference_files
 GTF_FILES_DIR=/data/users/kolsen/rna_seq/gtf_files
 
-# List all bam files should be assambled
+# List all bam files should be assembled
 FILES=($BAM_FILE_DIR/sorted_*.bam)
 
 # Get the filename of the sam-files
 FILENAME=$(basename "${FILES[$SLURM_ARRAY_TASK_ID]}" .bam | sed 's/sorted_//')
 
+# List all gtf files that should be merged
+GTF_FILES=($GTF_FILES_DIR/*.gtf)
+
+# Do transcriptome assembly with stringtie and merge the gtf files
 stringtie -o $GTF_FILES_DIR/$FILENAME.gtf -p 6 --rf -G $REFERENCE_FILE_DIR/gencode.v44.annotation.gff3 ${FILES[$SLURM_ARRAY_TASK_ID]}
+stringtie --merge -o $GTF_FILES_DIR/merged/merged.gtf -G $REFERENCE_FILE_DIR/gencode.v44.annotation.gff3 $GTF_FILES
